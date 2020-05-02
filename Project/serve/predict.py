4ad519c3b0ec3bf8a351397a9d3af0c9e61bbc36
EC2 Default User <ec2-user@ip-172-16-40-91.us-east-2.compute.internal>
2020-05-02 19:14:32 +0000

    Project 1: Sentiment Analysis

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,6 +72,8 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
+    input_data_words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_data_words)   
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +88,22 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    with torch.no_grad():
+        output = model.forward(data)
+        
+    result = np.round(output.numpy())
+    
+    print(result)
 
     return result
+
+def convert_and_pad_data(word_dict, data, pad=500):
+    result = []
+    lengths = []
+    
+    for sentence in data:
+        converted, leng = convert_and_pad(word_dict, sentence, pad)
+        result.append(converted)
+        lengths.append(leng)
+        
+    return np.array(result), np.array(lengths)
